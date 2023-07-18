a42c6ab01fc7d1c774ee0f9fb949451bc9739adb
Ivbermaster <ivbermaster@gmail.com>
2023-07-18 14:55:49 +0200

    tauri react

---
+++ frontend/music/src/App.tsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import NeuButton from './components/NeuButton'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+
+  return (
+    <>
+      <div id="container">
+        <NeuButton></NeuButton>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/music/src/components/NeuButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './componentscss/NeuButton.css';
+
+
+
+export default function NeuButton() {
+  return (
+    <button>Hi</button>
+  )
+}
